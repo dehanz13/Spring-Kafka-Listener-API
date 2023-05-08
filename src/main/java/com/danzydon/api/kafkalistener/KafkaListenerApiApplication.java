eb7f8e9e13583f01dd00b472998559b087633d6e
dehanz13 <hansel.danniel@gmail.com>
2023-05-07 19:08:07 -0500

    initial commit

---
+++ src/main/java/com/danzydon/api/kafkalistener/KafkaListenerApiApplication.java
@@ -0,0 +1,13 @@
+package com.danzydon.api.kafkalistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaListenerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaListenerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danzydon/api/kafkalistener/KafkaListenerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danzydon.api.kafkalistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaListenerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
